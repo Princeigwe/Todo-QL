6ed77cec2371a0be2784d47d284a7635b66907c1
Prince Igwe <igwep297@gmail.com>
2023-06-19 02:46:18 +0100

    created dummy object schema for user and binded resolvers to Query schema

--- schema.py
+++ schema.py
@@ -1,8 +1,14 @@
-from ariadne import QueryType, make_executable_schema, gql
+from ariadne import QueryType, make_executable_schema, gql, ObjectType
 
 type_defs = gql("""
     type Query {
         hello: String!
+        user: User!
+    }
+
+    type User {
+        firstName: String
+        lastName: String!
     }
 """)
 
@@ -14,5 +20,21 @@ query = QueryType()
 def resolve_hello(*_):
     return "Hello Prince"
 
+@query.field('user')
+def resolve_user(obj, info):
+    return user
+
+
+user = ObjectType('User')
+
+
+@user.field('firstName')
+def resolve_user_firstName(*_):
+    return "Prince"
+
+@user.field('lastName')
+def resolve_user_lastName(*_):
+    return "Igwe"
+
 # making the Python code schema executable in GraphQL
-schema = make_executable_schema(type_defs, query)
+schema = make_executable_schema(type_defs, query, user)
